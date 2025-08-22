96ad972a6758800c923b0bc8b633fe109ae70015
Camilo Spinelli <cspinelli@MacBook-Pro-de-Camilo.local>
2025-08-22 16:06:01 -0300

    added use cases in main app

--- app/src/main/java/com/example/countryfinder/MainApplication.kt
+++ app/src/main/java/com/example/countryfinder/MainApplication.kt
@@ -1,13 +1,20 @@
 package com.example.countryfinder
 
 import android.app.Application
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.preferencesDataStoreFile
+import com.example.countryfinder.data.favorites.FavoritesDataStore
 import com.example.countryfinder.data.repository.CityRepositoryImpl
 import com.example.countryfinder.data.services.CityApiService
+import com.example.countryfinder.domain.model.City
 import com.example.countryfinder.domain.repository.CityRepository
+import com.example.countryfinder.domain.usecase.ObserveFavoriteIdsUseCase
+import com.example.countryfinder.domain.usecase.ToggleFavoriteCityUseCase
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import kotlin.getValue
 
 /**
  * App's Composition Root.
@@ -15,6 +22,22 @@ import retrofit2.converter.gson.GsonConverterFactory
  */
 // TODO: Revisar si se puede pulir esto, no me convence
 class MainApplication : Application(), AppContainer {
+
+    companion object {
+        private const val BASE_URL = "https://gist.githubusercontent.com/hernan-uala/dce8843a8edbe0b0018b32e137bc2b3a/raw/"
+    }
+
+    private val dataStore by lazy {
+        PreferenceDataStoreFactory.create(
+            produceFile = { this.preferencesDataStoreFile("countryfinder_prefs") }
+        )
+    }
+
+    private val favoritesDataStore by lazy {
+        FavoritesDataStore(dataStore)
+    }
+
+    private val cityCache: Map<Int, City> = emptyMap()
 
     override val cityRepository: CityRepository by lazy {
         val logging = HttpLoggingInterceptor().apply {
@@ -32,10 +55,14 @@ class MainApplication : Application(), AppContainer {
             .build()
 
         val api = retrofit.create(CityApiService::class.java)
-        CityRepositoryImpl(api)
+        CityRepositoryImpl(favoritesDataStore, api, cityCache)
     }
 
-    companion object {
-        private const val BASE_URL = "https://gist.githubusercontent.com/hernan-uala/dce8843a8edbe0b0018b32e137bc2b3a/raw/"
+    override val toggleFavoriteCityUseCase: ToggleFavoriteCityUseCase by lazy {
+        ToggleFavoriteCityUseCase(cityRepository)
+    }
+
+    override val observeFavoriteIdsUseCase: ObserveFavoriteIdsUseCase by lazy {
+        ObserveFavoriteIdsUseCase(cityRepository)
     }
 }
